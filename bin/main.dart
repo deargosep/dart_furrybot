744e4d11efda350f5c16cd04a822c674fd2fe2ec
deargo <mchbtfl@gmail.com>
2022-11-15 15:10:37 +0500

    safety for group chats

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,7 @@
 import 'package:dart_furrybot/dart_furrybot.dart' as dart_furrybot;
 
 void main(List<String> arguments) {
-  dart_furrybot.startBot();
+  while (true) {
+    dart_furrybot.startBot();
+  }
 }
--- lib/dart_furrybot.dart
+++ lib/dart_furrybot.dart
@@ -11,41 +11,45 @@ void startBot() async {
   var env = DotEnv(includePlatformEnvironment: true)..load();
   var api = GPT3(env['openai']!);
   String BOT_TOKEN = env['bot']!;
-  try {
-    String initialPrompt =
-        "Мое имя - Фурри в пакетике. Я отношусь к субкультуре Фурри. Моя фурсона - Волк. Мой возраст - 18 лет. Мой создатель - Deargo. Я есть по ссылке vk.com. Мое фото есть по ссылке vk.com. Во мне более 400 строк. Моя сексуальная ориентация - бисексуальная.";
-    final username = (await Telegram(BOT_TOKEN).getMe()).username;
-    var teledart = TeleDart(BOT_TOKEN, Event(username!));
-    teledart.start();
+  String initialPrompt =
+      "Мое имя - Фурри в пакетике. Я отношусь к субкультуре Фурри. Моя фурсона - Волк. Мой возраст - 18 лет. Мой создатель - Deargo. Я есть по ссылке vk.com. Мое фото есть по ссылке vk.com. Во мне более 400 строк. Моя сексуальная ориентация - бисексуальная.";
+  final username = (await Telegram(BOT_TOKEN).getMe()).username;
+  var teledart = TeleDart(BOT_TOKEN, Event(username!));
+  teledart.start();
 
-    print('initialized! \n');
+  print('initialized! \n');
 
-    // init finished
+  // init finished
+  teledart.onMessage(entityType: 'bot_command', keyword: 'start').listen(
+      (message) => teledart.sendMessage(message.chat.id, 'hi there :3'));
 
-    teledart.onMessage(entityType: 'bot_command', keyword: 'start').listen(
-        (message) => teledart.sendMessage(message.chat.id, 'hi there :3'));
-    String lastMessage = initialPrompt;
-    teledart.onMessage().listen((event) async {
-      teledart.sendChatAction(event.chat.id, 'typing');
-      print(event.text);
-      var result = await api.completion(
-          // ${lastMessage != '' ? 'Friend: ${lastMessage.trim()}\n' : ''}
-          "You: ${event.text?.trim()}\nFriend:",
-          // temperature: 0,
-          maxTokens: 200,
-          stop: 'You:',
-          topP: 1,
-          temperature: 0.8,
-          frequencyPenalty: 0.5,
-          presencePenalty: 0);
-      List<int> codeUnits = result.choices[0].text.codeUnits;
-      String completed = Utf8Decoder().convert(codeUnits);
+  teledart.onMention().listen((event) => sendAIM(teledart, event, api));
 
-      print(completed);
-      lastMessage = completed;
-      event.reply(completed);
-    });
-  } catch (e) {
-    print('error');
-  }
+  String lastMessage = initialPrompt;
+  teledart.onMessage().listen((event) async {
+    if (event.chat.type == 'private') {
+      sendAIM(teledart, event, api);
+    }
+  });
+}
+
+void sendAIM(TeleDart teledart, TeleDartMessage event, GPT3 api) async {
+  teledart.sendChatAction(event.chat.id, 'typing');
+  print(event.text);
+  var result = await api.completion(
+      // ${lastMessage != '' ? 'Friend: ${lastMessage.trim()}\n' : ''}
+      "You: ${event.text?.trim()}\nFriend:",
+      // temperature: 0,
+      maxTokens: 200,
+      stop: 'You:',
+      topP: 1,
+      temperature: 0.8,
+      frequencyPenalty: 0.5,
+      presencePenalty: 0);
+  List<int> codeUnits = result.choices[0].text.codeUnits;
+  String completed = Utf8Decoder().convert(codeUnits);
+
+  print(completed);
+  // lastMessage = completed;
+  event.reply(completed);
 }
