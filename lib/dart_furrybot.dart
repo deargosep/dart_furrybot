077fe6e527c8c8b9ecee5203f1666fecc266cb12
deargo <mchbtfl@gmail.com>
2022-11-15 14:39:03 +0500

    try?

--- lib/dart_furrybot.dart
+++ lib/dart_furrybot.dart
@@ -11,36 +11,40 @@ void startBot() async {
   var env = DotEnv(includePlatformEnvironment: true)..load();
   var api = GPT3(env['openai']!);
   String BOT_TOKEN = env['bot']!;
-  String initialPrompt =
-      "Мое имя - Фурри в пакетике. Я отношусь к субкультуре Фурри. Моя фурсона - Волк. Мой возраст - 18 лет. Мой создатель - Deargo. Я есть по ссылке vk.com. Мое фото есть по ссылке vk.com. Во мне более 400 строк. Моя сексуальная ориентация - бисексуальная.";
-  final username = (await Telegram(BOT_TOKEN).getMe()).username;
-  var teledart = TeleDart(BOT_TOKEN, Event(username!));
-  teledart.start();
+  try {
+    String initialPrompt =
+        "Мое имя - Фурри в пакетике. Я отношусь к субкультуре Фурри. Моя фурсона - Волк. Мой возраст - 18 лет. Мой создатель - Deargo. Я есть по ссылке vk.com. Мое фото есть по ссылке vk.com. Во мне более 400 строк. Моя сексуальная ориентация - бисексуальная.";
+    final username = (await Telegram(BOT_TOKEN).getMe()).username;
+    var teledart = TeleDart(BOT_TOKEN, Event(username!));
+    teledart.start();
 
-  print('initialized! \n');
+    print('initialized! \n');
 
-  // init finished
+    // init finished
 
-  teledart.onMessage(entityType: 'bot_command', keyword: 'start').listen(
-      (message) => teledart.sendMessage(message.chat.id, 'hi there :3'));
-  String lastMessage = initialPrompt;
-  teledart.onMessage().listen((event) async {
-    teledart.sendChatAction(event.chat.id, 'typing');
-    print(event.text);
-    var result = await api.completion(
-        "${lastMessage != '' ? 'Friend: ${lastMessage.trim()}\n' : ''}You: ${event.text!.trim()}\nFriend:",
-        // temperature: 0,
-        maxTokens: 200,
-        stop: 'You:',
-        topP: 1,
-        temperature: 0.5,
-        frequencyPenalty: 0.5,
-        presencePenalty: 0);
-    List<int> codeUnits = result.choices[0].text.codeUnits;
-    String completed = Utf8Decoder().convert(codeUnits);
+    teledart.onMessage(entityType: 'bot_command', keyword: 'start').listen(
+        (message) => teledart.sendMessage(message.chat.id, 'hi there :3'));
+    String lastMessage = initialPrompt;
+    teledart.onMessage().listen((event) async {
+      teledart.sendChatAction(event.chat.id, 'typing');
+      print(event.text);
+      var result = await api.completion(
+          "${lastMessage != '' ? 'Friend: ${lastMessage.trim()}\n' : ''}You: ${event.text!.trim()}\nFriend:",
+          // temperature: 0,
+          maxTokens: 200,
+          stop: 'You:',
+          topP: 1,
+          temperature: 0.5,
+          frequencyPenalty: 0.5,
+          presencePenalty: 0);
+      List<int> codeUnits = result.choices[0].text.codeUnits;
+      String completed = Utf8Decoder().convert(codeUnits);
 
-    print(completed);
-    lastMessage = completed;
-    event.reply(completed);
-  });
+      print(completed);
+      lastMessage = completed;
+      event.reply(completed);
+    });
+  } catch (e) {
+    print('error');
+  }
 }
